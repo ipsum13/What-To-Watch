b0a4bdb6fc54ea1f6447b52b1f70a78b16fc0a9e
ipsum13 <samiul.lesum@outlook.com>
2020-06-06 11:22:23 -0500

    pre deployment

--- server/app.js
+++ server/app.js
@@ -44,4 +44,13 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 module.exports = app;
