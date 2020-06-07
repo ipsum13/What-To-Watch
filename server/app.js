57c5ad04d59d94e49237a74cae43d6bb167d50c1
ipsum13 <samiul.lesum@outlook.com>
2020-06-06 22:54:30 -0500

    changes made

--- server/app.js
+++ server/app.js
@@ -45,11 +45,12 @@ app.use(function(err, req, res, next) {
 });
 
 if (process.env.NODE_ENV === 'production') {
+  app.enable("trust proxy");
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static('/../client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, + "/../client/build/index.html"));
   });
 }
 
