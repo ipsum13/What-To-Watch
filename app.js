505014f93f3eb9b5d8eb59ee2e5943320f5b8179
ipsum13 <samiul.lesum@outlook.com>
2020-06-07 01:23:04 -0500

    added host

--- app.js
+++ app.js
@@ -50,9 +50,9 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve('client', 'build', 'index.html'));
   });
 }
-
+const host = '0.0.0.0';
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, host, () => console.log(`Server started on port ${PORT}`));
 
 // module.exports = app;
