bf53733dc2aa908b51446d1637f421815bd71341
ipsum13 <samiul.lesum@outlook.com>
2021-02-27 22:32:20 +0600

    commit

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+
 require("dotenv").config({ path: "./config.env" });
 const path = require('path');
+
 var createError = require('http-errors');
 var express = require('express');
 
@@ -24,26 +26,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/api/users', usersRouter);
 app.use('/api/movies', moviesRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  //res.render('error');
-});
 
 if(process.env.NODE_ENV == 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
